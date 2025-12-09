659d61d9f3c17769329c69748fea08404e4a9f0a
Дмитрий А. Мищуков <d.michukov@jusanmobile.kz>
2025-12-09 16:46:38 +0500

    Первый коммит сайта

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Плавная прокрутка
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
